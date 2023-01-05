f7d6d01bd16ff0a3a3524451de405c1ca356139c
retamozo <eretamozo97@gmail.com>
2023-01-05 13:35:05 -0300

    enabled cors

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = require('./routes');
-const cors = require("cors");
-const {
-  errorHandler,
-  logErrors,
-  boomErrorHandler,
-} = require('./middlewares');
-const expressDebug = require("express-debug")
+const cors = require('cors');
+const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares');
+const expressDebug = require('express-debug');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,22 +10,27 @@ const port = process.env.PORT || 3000;
 // I can't run both web server and the debugger on the same port. They are separate servers
 // it looks like the debugger is a server built in into the nodejs runtime
 // cross-env NODE_OPTIONS='--inspect'
-const whitelist = ["http://localhost:3000", "http://127.0.0.1:9229", "http://127.0.0.1:5500"];
+const whitelist = [
+  'http://localhost:3000',
+  'http://127.0.0.1:9229',
+  'http://127.0.0.1:5500',
+  'https://my-very-first-server-production.up.railway.app/',
+];
 
 const corsConfig = {
   origin: (origin, cb) => {
     if (whitelist.includes(origin)) {
-      cb(null, true)
+      cb(null, true);
     }
-  }
-}
-// app.use(cors(corsConfig))
+  },
+};
+app.use(cors(corsConfig))
 app.use(express.json());
 router(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-app.use(expressDebug)
+app.use(expressDebug);
 
 app.listen(port, () => {
   console.log('Mi port' + port);
