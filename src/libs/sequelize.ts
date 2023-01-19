f68e8b19f53ef7c1fb9324b84ab47eb76131cc67
retamozo <eretamozo97@gmail.com>
2023-01-19 01:08:13 -0300

    removes sync method from sequelize init

--- src/libs/sequelize.ts
+++ src/libs/sequelize.ts
@@ -1,17 +1,11 @@
+import config from "@config";
 import { setupModels } from "../db/models";
 import { Sequelize as SequelizeClient } from "sequelize";
-import config from "@config";
 
 const sequelize = new SequelizeClient(config.connectionURIString, {
   dialect: "postgres",
 });
 
 setupModels(sequelize);
-
-const sync = async () => {
-  await sequelize.sync();
-};
-
-sync();
 
 export default sequelize;
