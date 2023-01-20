7223ed4b92ec43718367e8cb684ae5ca60b0901c
retamozo <eretamozo97@gmail.com>
2023-01-19 22:12:26 -0300

    ⁂ syncing up models

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,8 +1,17 @@
 import { Sequelize } from "sequelize";
 import { Product, ProductSchema } from "./product.model";
 import { User, UserSchema } from "./user.model";
+import { Customer, CustomerSchema } from "./customer.model";
+import { Category, CategorySchema } from "./category.model";
 
 export function setupModels(sequelize: Sequelize) {
-  Product.init(ProductSchema, Product.config(sequelize)),
-    User.init(UserSchema, User.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
+  User.init(UserSchema, User.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize))
+
+  User.associate(sequelize.models)
+  Customer.associate(sequelize.models);
+  Category.associate(sequelize.models)
+  Product.associate(sequelize.models)
 }
