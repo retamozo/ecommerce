651525a36b1e96e581256cb3f9a6737439b5e115
retamozo <eretamozo97@gmail.com>
2023-01-26 13:26:05 -0300

    keep on track migrations

--- src/libs/umzug/umzug.ts
+++ src/libs/umzug/umzug.ts
@@ -14,4 +14,11 @@ export const umzug = new Umzug({
   storage: new SequelizeStorage({ modelName: "migrations", sequelize }),
 });
 
+(async () => {
+  // Checks migrations and run them if they are not already applied. To keep
+  // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
+  // will be automatically created (if it doesn't exist already) and parsed.
+  await umzug.up();
+})();
+
 export type Migration = typeof umzug._types.migration;
