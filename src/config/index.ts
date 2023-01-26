e0b11859a5880c14e35468845e5527b8a2869a4c
retamozo <eretamozo97@gmail.com>
2023-01-26 16:06:28 -0300

    updated env.EXAMPLE along config changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,33 +1,32 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 // pools will connect through env vars and parse automatically
 // https://github.com/brianc/node-postgres/tree/master/packages/pg-connection-string
 // TODO: figure out why this refuses to work. Output is "undefined"
 // const connectionString = parse('postgres://PGUSER:PGPASSWORD@PGHOST:PGPORT/PGDATABASE').client_encoding
 const {
-  PGUSER,
-  PGPASSWORD,
-  PGHOST,
-  PGPORT,
-  PGDATABASE,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DATABASE,
   NODE_ENV,
-  PORT
-} = process.env
+  PORT,
+} = process.env;
 
-const database = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`
+const database = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`;
 
 const config = {
   env: NODE_ENV || "DEV",
-  port: PORT || 3000,
-  dbUser: PGUSER,
-  dbPassword: PGPASSWORD,
-  dbHost: PGHOST,
-  dbName: PGDATABASE,
-  dbPort: PGPORT,
-  connectionURIString: database
-}
+  port: PORT || 3001,
+  dbUser: POSTGRES_USER,
+  dbPassword: POSTGRES_PASSWORD,
+  dbHost: POSTGRES_HOST,
+  dbName: POSTGRES_DATABASE,
+  dbPort: POSTGRES_PORT,
+  connectionURIString: database,
+};
 
-
-export default config
+export default config;
