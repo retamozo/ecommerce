0a3e59284e7dfc1ac30afe49cbdccc0a0ddf10e1
retamozo <eretamozo97@gmail.com>
2023-02-03 15:06:25 -0300

    ❌ WIP : error handler updated: omit unused parameters.

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -1,28 +1,49 @@
-import { conflict } from "@hapi/boom";
-import { ValidationError } from "sequelize";
+import { Boom, conflict, isBoom } from '@hapi/boom';
+import { NextFunction, Response, Request } from 'express';
+import { ValidationError } from 'sequelize';
 
-export const logErrors = (error, req, res, next) => {
+export const logErrors = (
+  error: Error,
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+): void => {
   next(error);
 };
 
-export const errorHandler = (error, req, res, next) => {
-  return res.status(500).json({
+export const errorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  res.status(500).json({
     error: error.message,
-    stack: error.stack,
+    stack: error.stack
   });
 };
 
-export const boomErrorHandler = (error, req, res, next) => {
-  if (error.isBoom) {
+export const boomErrorHandler = (
+  error: typeof Boom,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  if (isBoom(error)) {
     const { output } = error;
-    return res.status(output.statusCode).json(output.payload);
+    res.status(output.statusCode).json(output.payload);
   }
   next(error);
 };
 
-export const ormError = (error, req, res, next) => {
+export const ormError = (
+  error: ValidationError,
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
   if (error instanceof ValidationError) {
-    throw conflict(error.errors[0].message)
+    throw conflict(error.errors[0].message);
   }
-  next(error)
-}
+  next(error);
+};
