0b2933fe54128ee3530c490c73f0e789eb048b27
retamozo <eretamozo97@gmail.com>
2023-02-03 15:07:49 -0300

    WIP: update over the route orchestor.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,19 @@
-import productRouter from "@routes/products.router";
-import categoriesRouter from "@routes/categories.router";
-import usersRouter from "@routes/users.router";
-import customerRouter from "@routes/customers.router";
-import ordersRoute from "@routes/orders.router";
-import { Router } from "express";
+import productRouter from '@routes/products.router';
+import categoriesRouter from '@routes/categories.router';
+import usersRouter from '@routes/users.router';
+import customerRouter from '@routes/customers.router';
+import ordersRoute from '@routes/orders.router';
+import { Router, Express } from 'express';
 
-const router = (app) => {
+const router = (app: Express) => {
   const router = Router();
-  app.get("/", (_, res) => res.send("aloha"));
-  app.use("/api/v1", router);
-  router.use("/products", productRouter);
-  router.use("/categories", categoriesRouter);
-  router.use("/users", usersRouter);
-  router.use("/customers", customerRouter);
-  router.use("/orders", ordersRoute)
+  app.get('/', (_req, res) => res.send('Welcome to the ecommerce api. '));
+  app.use('/api/v1', router);
+  router.use('/products', productRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/customers', customerRouter);
+  router.use('/orders', ordersRoute);
 };
 
 export default router;
