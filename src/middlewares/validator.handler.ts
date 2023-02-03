470b39f91e4826fb250d4385cd2bbceb134b5e4e
retamozo <eretamozo97@gmail.com>
2023-02-03 15:05:14 -0300

    enforce types of the schema route validations

--- src/middlewares/validator.handler.ts
+++ src/middlewares/validator.handler.ts
@@ -1,13 +1,16 @@
-import { badRequest } from "@hapi/boom"
+import { FieldSource, SchemaToValidate } from '@custom-types/schemas';
+import { badRequest } from '@hapi/boom';
+import { NextFunction, Request, Response } from 'express';
 
-export const validatorHandler = (schema, property) => (req, res, next) => {
-  const field = req[property];
-  const { error } = schema.validate(field, {
-    abortEarly: false, // throw all possible erros in one shoot
-  });
-  if (error) {
-    next(badRequest(error));
-  }
-  next();
-};
-
+export const validatorHandler =
+  (schema: SchemaToValidate, property: FieldSource) =>
+  (req: Request, _res: Response, next: NextFunction) => {
+    const field = req[property];
+    const { error } = schema.validate(field, {
+      abortEarly: false // throw all possible erros in one shoot
+    });
+    if (error) {
+      next(badRequest(error.message));
+    }
+    next();
+  };
